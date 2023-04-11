5f7ded209c19bcb8364a8ceff7b8900756970131
Nayra Fracarolli Ferreira <nayrafracarolliferreira@gmail.com>
2023-04-11 13:45:36 -0300

    Finished Activity Payment

--- Primeiros Passos/Pagamento.js
+++ Primeiros Passos/Pagamento.js
@@ -1,4 +1,19 @@
-let vproduto;
+let vproduto = 100;
 let vpago;
-let opcao;
+let opcao = 3;
 
+switch (opcao)
+{
+    case 1:
+        console.log(vpago = vproduto * 0.9);
+        break;
+    case 2:
+        console.log(vpago = vproduto * 0.85);
+        break;
+    case 3:
+        console.log(vpago = vproduto);
+        break;
+    case 4:
+        console.log(vpago = vproduto * 1.1);
+        break;
+}
