de89d03cdda3f1c81852416f99464089a4b05dbb
Nayra <nayrafracarolliferreira@gmail.com>
2023-04-12 18:42:11 -0300

    My first functions

---
+++ Conhecendo Funções/Functions.js
@@ -0,0 +1,18 @@
+function SayMyName(Name)
+{
+    console.log('My name is ' + Name);
+}
+function Verification(Age)
+{
+    if (Age > 20)
+    {
+        console.log('To be age');
+    }
+    else
+    {
+        console.log('Underage');
+    }
+}
+
+SayMyName('Nayra');
+Verification(31);
