200bbe2edd21d43e02da6798aa0f2340561be517
Nayra <nayrafracarolliferreira@gmail.com>
2023-04-14 17:05:41 -0300

    Final Version

--- Criando Objetos e Classes/Objetos.js
+++ Criando Objetos e Classes/Objetos.js
@@ -1,10 +1,22 @@
 const nayra = {
     fname: 'Nayra',
     lname: 'Fracarolli Ferreira',
-    age: 31
+    age: 31,
+    describe: function ()    {
+        console.log(`My name is ${this.fname} and I am ${this.age}`);
+    }
 };
+
+/*nayra.heigth = 1.6;
+
+delete nayra.lname;
 
 console.log(nayra);
 console.log(nayra.lname);
 console.log(nayra.fname);
 console.log(nayra.age);
+console.log(nayra.heigth);*/
+
+nayra.describe();
+
+console.log(nayra['lname']);
