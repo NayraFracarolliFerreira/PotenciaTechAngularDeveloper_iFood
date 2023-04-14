e998d90ad93d80bf19d3ff8524da5f8afaedd360
Nayra <nayrafracarolliferreira@gmail.com>
2023-04-14 16:29:51 -0300

    Begin Learning Objects

---
+++ Criando Objetos e Classes/Objetos.js
@@ -0,0 +1,10 @@
+const nayra = {
+    fname: 'Nayra',
+    lname: 'Fracarolli Ferreira',
+    age: 31
+};
+
+console.log(nayra);
+console.log(nayra.lname);
+console.log(nayra.fname);
+console.log(nayra.age);
