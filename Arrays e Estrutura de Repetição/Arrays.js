f4eafbd1ae579f4bb08515c23954b9e8349ef4cb
Nayra <nayrafracarolliferreira@gmail.com>
2023-05-02 15:12:28 -0300

    Array1

---
+++ Arrays e Estrutura de Repetição/Arrays.js
@@ -0,0 +1,27 @@
+let dog = ['Cocada','Raj'];
+console.log(dog);
+dog.push('s');
+console.log(dog);
+console.log(dog.pop());
+console.log(dog);
+dog.push('Rajib');
+console.log(dog);
+dog[1] = 'Paloma';
+console.log(dog);
+dog.push('Raj');
+console.log(dog);
+
+let bambucha = ['Cofap', 'Raj', 'José Roberto', 'Nayra', 'Dulce'];
+console.log(bambucha);
+console.log(bambucha.shift());
+console.log(bambucha);
+
+let amigas = [];
+amigas.push('Nayra');
+console.log(amigas);
+amigas.push('Caroline');
+console.log(amigas);
+amigas[1] = 'Carina';
+console.log(amigas);
+amigas[2] = 'Caroline';
+console.log(amigas);
